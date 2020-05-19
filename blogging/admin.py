d125a5cb8aa849c4f02d30466c276f5bbe09af26
Toogii2019 <dashdava@uw.edu>
2020-05-19 10:52:18 -0700

    Admin page done

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,6 +2,29 @@ from django.contrib import admin
 from polling.models import Poll
 from blogging.models import Category, Post
 
+
+class MembershipInline(admin.TabularInline):
+    model = Category.posts.through
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [MembershipInline,]
+    exclude = ('posts',)
+
+
+
+admin.site.site_header = "Django Blog Admin"
+admin.site.site_title = "Django Blog Title"
+admin.site.index_title = "Django Blog Index"
+
+#admin.site.register(PostAdmin)
+#admin.site.register(CategoryAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
 admin.site.register(Poll)
-admin.site.register(Post)
-admin.site.register(Category)
+#admin.site.register(Post)
+#admin.site.register(Category)
